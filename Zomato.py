384126a40ea8ea10ec1ed3fcdd4fe8330d296fea
Anandhi_R <154721121+Anandhi0817@users.noreply.github.com>
2024-03-31 12:33:08 +0530

    Zomato.py

--- Zomato.py
+++ Zomato.py
@@ -1,7 +1,7 @@
 # Importing libraries
-!pip install plotly
-!pip install pandas
-!pip install streamlit 
+pip install plotly
+pip install pandas
+pip install streamlit 
 import plotly.graph_objects as go
 import plotly.express as px
 import streamlit as st
